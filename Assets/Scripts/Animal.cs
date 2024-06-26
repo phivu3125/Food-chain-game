2969f934295fad975e35ea6285076be6269377b7
phivu3125 <phivu3125@gmail.com>
2024-06-26 14:51:08 +0700

    add animation

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -11,63 +11,55 @@ public class Animal : MonoBehaviour
     public String[] _listAnimalEat;
     public String[] _listAnimalPoisonnedBy;
     private bool _canRun = false;
-    [SerializeField]
-    private int _maxHealth;
+    private bool _isBeingAttacked = false;
+    private bool _isAttackedByPeer = false;
+
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private Animator _animator;
     private int _health;
     private HealthBar _healthBar;
-
+    private GameObject _targetAnimalDestroy = null;
     public bool CanRun { get => _canRun; set => _canRun = value; }
+
     private void Start()
+    {
+        InitializeHealth();
+    }
+
+    private void InitializeHealth()
     {
         _health = _maxHealth;
         _healthBar = transform.Find("Health")?.Find("HealthBar")?.GetComponent<HealthBar>();
-        _healthBar?.SetMaxHealth(20);
-        _healthBar?.SetHealth(20);
+        _healthBar?.SetMaxHealth(_maxHealth);
+        _healthBar?.SetHealth(_maxHealth);
         _healthBar?.gameObject.SetActive(false);
     }
 
-    // private void Start() {
-    //     _healthBar = transform.Find();
-    // }
-
-    // private void OnEnable(){
-    //     this.Register(EventID.ANIMAL_EAT_ENEMY, oneatanimal);
-    // }
-
-    // private void oneatanimal(object obj)
-    // {
-    //     throw new NotImplementedException();
-    // }
-
-    // private void OnDisable() {
-    //     this.Unregister(EventID.ANIMAL_EAT_ENEMY, oneatanimal);
-    // }
-
-    // private void Start() {
-    //     // _healthBar = gameObject.GetChildre
-    // }
-
     private void Update()
     {
-        if (_canRun && gameObject.CompareTag("AnimalPlayer"))
-        {
-            transform.position += (Vector3.right * moveSpeed) * Time.deltaTime;
-            if (transform.position.x > 20)
-            {
-                Destroy(gameObject);
-            }
-        }
-        if (gameObject.CompareTag("AnimalEnemy"))
-        {
-            transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
-            if (transform.position.x < -10)
-            {
-                Destroy(gameObject);
-            }
-        }
-        if (_health <= 0)
+        HandleMovement();
+        CheckHealth();
+    }
+
+    private void HandleMovement()
+    {
+        if (!_canRun) return;
+
+        Vector3 direction = gameObject.CompareTag("AnimalPlayer") ? Vector3.right : Vector3.left;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x > 20 || transform.position.x < -15)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void CheckHealth()
+    {
+        if (_health <= 0)
+        {
+            _isBeingAttacked = true;
+            StartCoroutine(Die());
         }
     }
 
@@ -84,36 +76,55 @@ public class Animal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "AnimalEnemy" && gameObject.tag == "AnimalPlayer")
+        if (IsEnemyCollision(collision, out GameObject enemyAnimal))
         {
-            // Check for Animal Player
-            GameObject animalEnemy = collision.gameObject;
+            HandleEnemyCollision(enemyAnimal);
+        }
+    }
 
-            if (_listAnimalEat.Contains(animalEnemy.name))
+    private bool IsEnemyCollision(Collider2D collision, out GameObject enemyAnimal)
+    {
+        enemyAnimal = null;
+        if ((collision.gameObject.tag == "AnimalEnemy" && gameObject.tag == "AnimalPlayer") ||
+            (collision.gameObject.tag == "AnimalPlayer" && gameObject.tag == "AnimalEnemy"))
+        {
+            enemyAnimal = collision.gameObject;
+            return true;
+        }
+        return false;
+    }
+
+    private void HandleEnemyCollision(GameObject enemyAnimal)
+    {
+        Animal enemyAnimalComponent = enemyAnimal.GetComponent<Animal>();
+        if (_listAnimalEat.Contains(enemyAnimal.name) && !enemyAnimalComponent._isBeingAttacked)
+        {
+            // If 2 animal can eat each other
+            if (enemyAnimalComponent._listAnimalEat.Contains(gameObject.name) && !enemyAnimalComponent._isAttackedByPeer)
             {
-                Destroy(animalEnemy);
+                enemyAnimalComponent._isBeingAttacked = true; // to block others enemies attack
+                enemyAnimalComponent._isAttackedByPeer = true; // to block others same level enemies attack
+                PrepareForAttack(gameObject);
             }
-            else if (_listAnimalPoisonnedBy.Contains(animalEnemy.name))
+            else if (!_isBeingAttacked) // to block this animal attack others enemies when dying
             {
-                Destroy(animalEnemy);
-                StartCoroutine(ReduceHealthOverTime(5, 4));
+                enemyAnimalComponent._isBeingAttacked = true;
+                PrepareForAttack(enemyAnimal);
             }
         }
-        else if (collision.gameObject.tag == "AnimalPlayer" && gameObject.tag == "AnimalEnemy")
+        else if (_listAnimalPoisonnedBy.Contains(enemyAnimal.name) && !enemyAnimalComponent._isBeingAttacked)
         {
-            // Check for Animal Enemy
-            GameObject animalPlayer = collision.gameObject;
-            if (_listAnimalEat.Contains(animalPlayer.name))
-            {
-                Destroy(animalPlayer);
-            }
-            else if (_listAnimalPoisonnedBy.Contains(animalPlayer.name))
-            {
-                Destroy(animalPlayer);
-                StartCoroutine(ReduceHealthOverTime(5, 4));
-            }
-
+            enemyAnimalComponent._isBeingAttacked = true;
+            PrepareForAttack(enemyAnimal);
+            StartCoroutine(ReduceHealthOverTime(5, 4));
         }
+    }
+
+    private void PrepareForAttack(GameObject enemyAnimal)
+    {
+        _canRun = false;
+        _targetAnimalDestroy = enemyAnimal;
+        _animator?.SetTrigger("isAttacking");
     }
 
     private IEnumerator ReduceHealthOverTime(int damagePerTick, float interval)
@@ -136,9 +147,40 @@ public class Animal : MonoBehaviour
         }
     }
 
-
-
-    // private void EatEnemy(){
-    //     this.Broadcast(EventID.ANIMAL_EAT_ENEMY, this);
-    // }
+    public void DestroyTargetAnimal()
+    {
+        if (_targetAnimalDestroy != null)
+        {
+            Animal targetAnimalComponent = _targetAnimalDestroy.GetComponent<Animal>();
+            if (targetAnimalComponent._listAnimalEat.Contains(gameObject.name))
+            {
+                if (targetAnimalComponent._targetAnimalDestroy != null)
+                {
+                    StartCoroutine(Die());
+                }
+            }
+            else
+            {
+                StartCoroutine(targetAnimalComponent?.Die());
+            }
+            if (_targetAnimalDestroy.Equals(gameObject)) // check if target animal is gameobject, set it can't run
+            {
+                _canRun = false;
+            }
+            else
+            {
+                _canRun = true;
+            }
+            _targetAnimalDestroy = null;
+        }
+    }
+    private IEnumerator Die()
+    {
+        _canRun = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        _animator?.SetTrigger("isDying");
+        transform.position = new Vector3(transform.position.x, transform.position.y, 1);
+        yield return new WaitForSeconds(_animator.GetCurrentAnimatorStateInfo(0).length);
+        Destroy(gameObject);
+    }
 }
